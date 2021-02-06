b75f82e3295d03ffa2a2b082e9dd689976339ee2
Novak <32582512+kelvinndmo@users.noreply.github.com>
2021-02-06 22:26:23 +0300

    Update MapPreview.tsx

--- components/MapPreview.tsx
+++ components/MapPreview.tsx
@@ -4,7 +4,7 @@ import { Image, StyleSheet, View, TouchableOpacity } from "react-native";
 const MapPreview = (props) => {
   let imagePreviewUrl;
   if (props.location) {
-    imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${props.location.lat},${props.location.lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:A%7C${props.location.lat},${props.location.lng}&key=AIzaSyBq65urONoFNYnQiwnl-vkXAdLa_HHfRnw`;
+    imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${props.location.lat},${props.location.lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:A%7C${props.location.lat},${props.location.lng}&key=novak`;
   }
   return (
     <TouchableOpacity
